2af8a618bc369c678b0d10e02a216670f89e79ca
EduardoSantos-2020 <eduardosantos123444@gmail.com>
2025-12-09 22:14:58 -0300

    teste 9 IOF

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@ lastDayMonth=new Date(ano, mes , 0);
 
 const horaAtual = date.toLocaleTimeString();
 
+// caso for dia 1 antes do meio dia retorna para utimo dia do mes
 if (diaAtual==1 && horaAtual<='12:00:00') {
    diaAtual = lastDayMonth.getDate();
 }
@@ -15,29 +16,32 @@ diaAtual >= 1 && diaAtual <= 9 ? diaAtual = '0' + diaAtual : diaAtual
 
 dataAtual = `${ano}-${mes + 1}-${diaAtual}`;
 
-if (final === 6) { //Para o dia no Sabado
+//Para o dia no Sabado//////////////////
+if (final === 6) { 
     diaAtual -= 1
 }
+////////////////////////////////////////
 
-if (final === 0 && horaAtual >= '00:00:00' && horaAtual <= '23:59:00') { //Para o dia no Domingo
+
+//Para o dia no Domingo/////////////////
+if (final === 0 && horaAtual >= '00:00:00' && horaAtual <= '23:59:00') { 
     diaAtual -= 2
 }
+/////////////////////////////////////////
 
-
-
-// Para o dia Segunda-Feira antes dos 12 dia.
+// Para o dia Segunda-Feira depois das cinco horas da tarde 
 if (final === 1 && horaAtual <= '17:12:00' ) {
     diaAtual -= 3
 }
 
-// Para o dias Terça a Sexta antes das 12 meia noite e 10 horas da manhãm.
-if (final >= 2 && final <= 5 && horaAtual >= '00:00:00' && horaAtual <= '10:12:00') {
+// Para o dias Terça a Sexta depois meia noite e depois do meio dia.
+if (final >= 2 && final <= 5 && horaAtual >= '00:00:00' && horaAtual <= '12:12:00') {
     diaAtual -= 1
 }
 
-// Para o dias Terça a Sexta antes das 12 meio dia e 5 horas da tarde.
+// Para o dias Terça a Sexta depois do meio dia e antes das cinco horas da tarde.
 if (final >= 2 && final <= 5 && horaAtual >= '12:12:00' && horaAtual <= '17:12:00') {
-    diaAtual = diaAtual     
+    diaAtual = diaAtual
 }
 
 
